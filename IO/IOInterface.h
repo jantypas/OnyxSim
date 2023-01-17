696a0ee1638449f6a3fa67c41c2d4f8bc904a286
jantypas <ja@antypas.net>
2023-01-16 22:15:17 -0800

    1/16/2023 Added basic IO descripters

--- IO/IOInterface.h
+++ IO/IOInterface.h
@@ -1,10 +1,13 @@
 //
 // Created by jantypas on 1/8/23.
 //
-
+#include <stdint-gcc.h>
 #ifndef ONYXSIM_IOINTERFACE_H
 #define ONYXSIM_IOINTERFACE_H
 
+struct IOHandle {
+    uint64_t   handle;
+};
 
 class IOInterface {
 
--- Processes/Process.h
+++ Processes/Process.h
@@ -5,6 +5,8 @@
 #ifndef ONYXSIM_PROCESS_H
 #define ONYXSIM_PROCESS_H
 #include <string>
+#include <vector>
+#include "../IO/IOInterface.h"
 
 #define SEGMENT_STATE_IN_USE        0x0001
 #define SEGMENT_STATE_IS_LOCKED     0x0002
@@ -15,7 +17,7 @@
 #define PRIV_LEVEL_DRIVER   0x02
 #define PRIV_LEVEL_KERNEL   0x03
 
-class ProcessSegmen {
+class ProcessSegment {
 public :
     uint16_t        processID;
     std::string     name;
@@ -26,14 +28,18 @@ public :
 
 class Process {
 public :
-    uint32_t        userid;
-    uint32_t        groupid;
-    std::string     commandline;
-    uint8_t         numSegments;
-    ProcessSegmen   *segments;
-    uint64_t        *dataRegisters;
-    uint64_t        *addressRegisters;
-    uint8_t         privLevel;
+    uint32_t                userid;
+    uint32_t                groupid;
+    std::string             commandline;
+    uint8_t                 numSegments;
+    ProcessSegment         *segments;
+    uint64_t               *dataRegisters;
+    uint64_t               *addressRegisters;
+    uint8_t                 privLevel;
+    IOHandle                handleStdin;
+    IOHandle                handleStdout;
+    IOHandle                handleStdError;
+    std::vector<IOHandle>   handleFiles;
 };
 
 
